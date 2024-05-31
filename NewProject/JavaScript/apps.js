2e579273307624ed8640f0244534f5993e5033f9
HegdeVinay12 <bavinay99@gmail.com>
2024-05-31 18:14:56 +0530

    modifield

--- NewProject/JavaScript/apps.js
+++ NewProject/JavaScript/apps.js
@@ -1,4 +1,12 @@
-function createDatabase() {
-  var Database_Name = 'MyDatabase';
-  var version = 1.0;
-}
+// function createDatabase() {
+//   var Database_Name = 'MyDatabase';
+//   var version = 1.0;
+// }
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+
+menu.addEventListener('click' function{
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+})
